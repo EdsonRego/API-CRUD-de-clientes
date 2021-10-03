b0ff9a6c6652560948b2dbd15e1352f265fd28aa
EdsonRego <edsonxrego@gmail.com>
2021-10-02 22:38:57 -0300

    Create Projetc Backend

---
+++ backend/src/main/java/com/edsonrego/apicrudclientes/ApiCrudDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.edsonrego.apicrudclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudDeClientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/edsonrego/apicrudclientes/ApiCrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonrego.apicrudclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
