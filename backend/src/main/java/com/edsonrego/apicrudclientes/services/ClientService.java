d8e80ff43a58b443dc7cb71a38aa34d294ca9882
EdsonRego <edsonxrego@gmail.com>
2021-10-07 22:16:13 -0300

    Database seeding

--- backend/src/main/java/com/edsonrego/apicrudclientes/services/ClientService.java
+++ backend/src/main/java/com/edsonrego/apicrudclientes/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edsonrego.apicrudclientes.entities.Client;
 import com.edsonrego.apicrudclientes.repositories.ClientRepository;
@@ -14,6 +15,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Client> findAll(){
 		return repository.findAll();
 	}
