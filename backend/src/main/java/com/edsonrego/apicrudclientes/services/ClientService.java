a8f96ccd854f7caa99f50f4c33c5eec4ec693253
EdsonRego <edsonxrego@gmail.com>
2021-10-07 23:04:06 -0300

    DTO

--- backend/src/main/java/com/edsonrego/apicrudclientes/services/ClientService.java
+++ backend/src/main/java/com/edsonrego/apicrudclientes/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.edsonrego.apicrudclientes.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.edsonrego.apicrudclientes.dto.ClientDTO;
 import com.edsonrego.apicrudclientes.entities.Client;
 import com.edsonrego.apicrudclientes.repositories.ClientRepository;
 
@@ -16,7 +18,8 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());	
 	}
 }
